afda6bebe9d3eef71222d62324623fade99f8ed2
Cristianv9 <156293750+Cristianv9@users.noreply.github.com>
2025-01-23 19:43:37 +0000

    Confetti

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+let confetti = new Confetti('confetti');
+console.log(confetti);
 
+confetti.destroyTarget(false); 
+confetti.setSize(10);
+confetti.setCount(100);
+confetti.setPower(100);
 console.log('👍🏻 okay');
